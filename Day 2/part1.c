2ea3d780cb4a14257e910534857cbbdbefe22cc3
Roko <83535959+inzzzy@users.noreply.github.com>
2021-12-07 15:37:46 +0100

    Create part1.c

---
+++ Day 2/part1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SAMPLE_FILE "sample.txt"
+#define INPUT_FILE "input.txt"
+#define BUFFER_SIZE 256
+
+int main() {
+    FILE *fp = fopen(INPUT_FILE, "r");
+    if (!fp) {
+        fprintf(stderr, "ERROR: Could not open file.\n");
+        return 1;
+    }
+
+    int h = 0;
+    int d = 0;
+
+    char cmd[BUFFER_SIZE];
+    int x;
+
+    while(fscanf(fp, "%s %d", &cmd, &x) == 2){
+        if (strcmp(cmd, "forward") == 0) h += x;
+        if (strcmp(cmd, "down") == 0) d += x;
+        if (strcmp(cmd, "up") == 0) d -= x;
+    }
+
+    int ans = h * d;
+    printf("Pos: %d\n", ans);
+    fclose(fp);
+  
+}
